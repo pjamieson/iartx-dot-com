419c3214423bbbfc954da90261f8e18b88cd94a9
Patrick Jamieson <patrick@cleancodecorp.com>
2023-04-14 14:16:20 -0700

    Update gossen-typewriter.js

--- src/pages/typewriters/gossen-typewriter.js
+++ src/pages/typewriters/gossen-typewriter.js
@@ -73,51 +73,67 @@ const GossenTypewritersPage = ({ data }) => {
   <div className="historical-text">
     <h2>A Brief History of the Gossen Tippa Portable Typewriter Models, with a glance at Adler and Triumph Tippas</h2>
 
-    <p>The company P. Gossen & Co. K.-G was founded in 1919 by the then 46-year-old German engineer, Paul Gossen. The new firm set about manufacturing and selling electrical measuring instruments. An early product—called the <em>Millivoltmeter</em>—was the first device offered for measuring voltage in the millivolt range. The company was successful, and expanded through the 1920s and 1930s. However, from 1939 into 1945—during World War II—the company was required to switch to the production of electrical instruments needed by the German armed forces.</p>
+    <p>The company P. Gossen & Co. K.-G was founded in Germany in 1919 by Paul Gossen, a 46-year-old engineer. The new firm set about manufacturing and selling electrical measuring instruments. An early product—called the <em>Millivoltmeter</em>—was the first device offered for measuring voltage in the millivolt range. The company was successful, and expanded through the 1920s and 1930s. However, from 1939 into 1945—during World War II—the company was required to dedicate themselves to the provision of electrical instruments needed by the German armed forces.</p>
     
-    <p>Paul Gossen had passed away during the war—in June 1942—and the company was inherited by his widow and children. Paul's son Hans Gossen, and his son-in-law, Karl Klarner, became the chief executives. In August 1945, the U.S. Group Control Council, Germany (USGCC) allowed the company to resume manufacturing its pre-war product line. Plans to expand into the production of typewriters were accelerated—if not initiated—at that time as well.</p>
+    <p>Paul Gossen passed away during the war—in June 1942—and the company was inherited by his widow and children. Paul's son Hans Gossen, and his son-in-law, Karl Klarner, became the chief executives. In August 1945, the U.S. Group Control Council, Germany (USGCC) allowed the company to resume manufacturing its pre-war product line. Plans to expand into the production of typewriters were accelerated—if not initiated—at that time as well.</p>
 
     <h3>1948 — Gossen Launches Their First Typewriter</h3>
 
     <p>The chief designer of Gossen's first typewriter, the <b>Gossen Tippa</b>, was Erwin Pfaffenberger who had joined the company in 1924 as a trainee draughtsman. Pfaffenberger apparently strived to improve upon the <em>Hermes Baby</em>, which had been on the market since 1935 and was the first widely-distributed <em>ultra-portable</em> typewriter. When the Gossen Tippa was introduced in 1948, it had a nearly-identical footprint to that of the Baby. It had a similar metal lid that also was secured at the back of the machine with two short metal studs, and it was 3/8 inch shorter than the Baby at 2 1/4 inches (or 2 7/16 inches with the lid attached). At 8 pounds 11 ounces, including the lid, the Tippa weighed a mere 2 ounces more than the Baby.</p>
 
-    <p>I am a fan and user of both the Gossen Tippa and the Hermes Baby, although I slightly prefer the feel of my Tippa. However—for me—each has its own application: I take the Tippa or a Baby when roaming, but I employ a second generation <em>Hermes 3000</em> on my desktop at home. My Baby keyboards provide diacritics for writing in French, Spanish, or Portuguese that my Tippa lacks. That bumps a Baby up to my first choice when I want those diacritics while roaming. Your own mileage may vary.</p>
+    <p>I am a fan and user of both the Gossen Tippa and the Hermes Baby, although I slightly prefer the feel of my Tippa. However, I find each has its own application; I take the Tippa or a Baby when roaming, but I employ a second generation <em>Hermes 3000</em> on my desktop at home. A Baby keyboard I have provides diacritics for writing in French, Spanish, or Portuguese, a feature that my Tippa keyboard lacks. That feature bumps my Baby up to my first choice when I want those diacritics while roaming. Your own mileage may vary.</p>
     
-    <p>Typewriter collector Paolo Dal Chiele identifies the first two models of the Gossen Tippa as the <em>Tippa A Mk. I</em> and the <em>Tippa A Mk. II</em>. He notes distinguishing features of the Mk. I include its round, glass key tops, and two lid release buttons, one centered on each side of the lid. This model was manufactured into 1950 until it was replaced by the Mk. II. That second model has plastic, tombstone-shaped key tops and a single release button centered on the front end of the lid. The Mk. II was produced into 1953.</p>
+    <p>Typewriter collector Paolo Dal Chiele identifies the first two models of the Gossen Tippa as the <em>Tippa A Mk. I</em> and the <em>Tippa A Mk. II</em>. He notes distinguishing features of the Mk. I include its round, glass key tops, and two lid release buttons, one centered on each side of the lid. This model was manufactured into 1950, when it was replaced by the Mk. II. That second model has plastic, tombstone-shaped key tops and a single release button centered on the front end of the lid. The Mk. II was produced into 1953.</p>
 
-    <p>A Gossen Tippa promotional brochure—illustrating the Mk. II model—noted that the typefaces offered were Pica and Elite, and the colors available were black, cerulean blue, light grey, maroon, sand, and ivory white. The listed price was then DM325 [US$77.50] (with metal lid), plus DM70 [US$17] for the optional leather briefcase.</p>
+    <p>A Gossen Tippa promotional brochure—illustrating the Mk. II model—noted that the typefaces offered were Pica and Elite, and the colors available were black, cerulean blue, light grey, maroon, sand, and ivory white. The listed price with metal lid was then DM325 [US$77.50 (= US$883 in 2023)], plus DM70 [US$17 (= US$194 in 2023)] for the optional leather briefcase. Extended payment plans were offered.</p>
 
-    <p>A new model—often identified as the <b>Gossen Tippa B</b>—appeared in 1953. Although we have seen one with a metal lid, the Tippa B generally replaced the earlier base and lid with a base/lid combination that has a small leather suitcase look. This lockable case provides storage room in the lid. Over the ribbons, a pair of gull-wing covers with rounded tips replaced the earlier one-piece cover, and the ribbon/stencil selector was eliminated. Initially, the Tippa B retained the original Tippa's small, rotating carriage return lever, but in early 1954 that lever was replaced with a longer, easier-to-use one.</p>
+    <p>A new model—often identified as the <b>Gossen Tippa B</b>—appeared in 1953. Although it occasionally came with a metal lid, the Tippa B typically replaced the earlier base and lid with a base/lid combination that had a small leather suitcase look. This lockable case provides storage room in the lid. Over the ribbons, a pair of gull-wing covers with rounded tips replaced the earlier one-piece cover, and the ribbon/stencil selector was eliminated. Initially, the Tippa B retained the original Tippa's small, rotating carriage return lever, but in early 1954 that lever was replaced with a longer, easier-to-use one.</p>
 
     <p>In 1955, an evolved Tippa version named the <b>Gossen Tippa Pilot</b> appeared. In addition to the <em>Gossen Tippa</em> nameplate on the top of the shell, below the right ribbon cover, a chromed metal <em>pilot</em> logo was added below the left ribbon cover. A ribbon/stencil selector was again included. The Pilot model would prove to be the last in Gossen's line of typewriters. </p>
 
     <h3>Gossen Tippa Case Options — The Office in a Briefcase</h3>
 
-    <p>Different case options were offered for Gossen Tippas from the beginning. Initially, the most common was the standard metal lid that attaches directly to the machine. The color of that lid typically matches the color of the typewriter itself. Inside the metal lid, there is no room for storage, but there are metal clips mounted on both sides of the base to secure writing instruments or cleaning brushes. Gossen offered an optional, well-made, branded leather briefcase with a compartment that the typewriter (with its metal lid attached) slides into, and another compartment to hold fresh paper and typed sheets.</p>
+    <p>From the beginning, Gossen offered different case options for its Tippas. Initially, the most common was the standard metal lid attaching directly to the machine. The color of that lid typically matched the color of the typewriter itself. Inside the metal lid, while there was no room for storage, metal clips were mounted on both sides of the base to secure writing instruments or cleaning brushes. Gossen also offered an optional, well-made, branded leather briefcase with a compartment that held the typewriter (with its metal lid attached). A second compartment held fresh paper, typed sheets, and other supplies.</p>
 
-    <p>The suitcase-style leather case came in several colors, and included (usually) a burgundy-colored folder attached inside the case lid to hold paper and/or the instructions pamphlet. Branding printed in white on that folder reads <em>Tippa-Boy</em> (In the Typewriter Database, this Tippa-Boy case branding shows up at least three times with a Tippa A Mk. II machine.), <em>Tippa-B</em>, or <em>Tippa necessaire</em> (seen in the Typewriter Database, mostly with Tippa Pilot machines).</p>
+    <p>The suitcase-style leather case came in several colors, and included a folder attached inside the removable case lid to hold paper and supplies. Branding printed on that folder reads <em>Tippa-Boy</em>, <em>Tippa-B</em>, or <em>Tippa necessaire</em>.</p>
 
     <h3>Autumn 1956 — Gossen Sells Tippa License to Adler</h3>
 
     <p>It appears that Gossen's typewriter sales peaked in 1954 with about 35,000 units being produced that year. In the Autumn of 1956—following nearly two years of declining typewriter sales—the company apparently decided to re-focus their business on their electrical instruments, and they sold the license for <em>Tippa</em> typewriters to <em>Adler</em>.</p>
 
-    <p>Writer and typewriter collector Robert Messenger notes subsequent typewriter manufacturer acqusitions and mergers involving the Tippa: <em>Grundig</em> took over the <em>Triumph</em> typewriter brand in 1957, and then in 1966, the <em>Grundig-owned Triumph</em> company took control of <em>Adler</em>. Finally, in 1969, <em>Litton Industries</em> took over <em>Triumph-Adler</em>.</p>
+    <p>Writer and typewriter collector Robert Messenger notes subsequent typewriter manufacturer acqusitions and mergers involving the Tippa:
+    </p>
 
-    <p>There are several 1957 and 1958 <b>Adler Tippa</b> and <b>Triumph Tippa</b> galleries in the Typewriter Database. These typewriters look exactly like the Gossen Tippa B, but have <em>Adler Tippa</em> or <em>Triumph Tippa</em> nameplates below their right ribbon covers rather than <em>Gossen Tippa</em> ones. They have metal lids, and one also has the leather breifcase.</p>
+      <ul>
+        <li>1957 - Grundig absorbs Triumph typewriter brand</li>
+        <li>1966 - Grundig-owned Triumph takes control of Adler</li>
+        <li>1969 - Litton Industries takes over Triumph-Adler</li>
+      </ul>
+    
+    <p>There are several 1957 and 1958 <b>Adler Tippa</b> and <b>Triumph Tippa</b> galleries in the Typewriter Database. [See the TypewriterDatabase.com links in the footnotes.] These typewriters look exactly like the Gossen Tippa B, but have <em>Adler Tippa</em> or <em>Triumph Tippa</em> nameplates below their right ribbon covers rather than <em>Gossen Tippa</em> ones. They have metal lids, and one in the database also has the leather breifcase.</p>
 
     <h3>Tippa Typewriters after Gossen: Adler and Triumph</h3>
 
-    <p>1959 saw the launch of the first Adler-designed-and-manufactured Tippa. (Adler had already been manufacturing other typewriter models in Germany for over 60 years.) Their Tippa had a new rounded look, with its design winning awards in Milan in 1960 and in Germany in 1962. The two-piece shell was plastic. Slightly-evolved models, called the <b>Adler Tippa 1</b> and the <b>Adler Tippa 2</b> appeared in 1964 and 1966. A boxy-looking model, the <b>Adler Tippa S</b>, was manufactured in Holland by the merged Triumph-Adler and launched in 1967. That model—with production moving to Japan in the early 1970s—was offered into 1976.</p>
-
-    <p>The earliest <em>Triumph</em> Tippa I have seen (other than a re-branded Gossen Tippa B) appears to be a re-branded, rounded-look <em>Adler</em> Tippa manufactured in 1961. A <b>Triumph Tippa 1</b>-branded model—looking very much like an <em>Adler</em> Tippa 1—appeared in 1965. Japan-made, plastic, <b>Triumph Tippa S</b> and <em>Triumph</em> Tippa-branded machines—looking very much like the <em>Adler</em> Tippa S—were manufactured from the early to the late 1970s.</p>
+    <p>1959 saw the launch of the first Adler-designed-and-manufactured Tippa. (Adler had been manufacturing other typewriter models in Germany for over 60 years.) Their Tippa had a new rounded look, winning design awards in Milan in 1960 and in Germany in 1962. The two-piece shell was plastic. Models, called the <b>Adler Tippa 1</b> and the <b>Adler Tippa 2</b> appeared in 1964 and 1966. A boxy-looking model, the <b>Adler Tippa S</b>, was manufactured in Holland by the merged Triumph-Adler and launched in 1967. That model—with production moving to Japan in the early 1970s—was offered into 1976.</p>
 
     <p>Theodore Munk, the mastermind behind the Typewriter Database, notes that—under <em>Royal/Litton</em>—the Tippa S continued in the form of the <b>Royal Caravan</b> and the <b>Royal Sahara</b>. These plastic machines were made in Holland begining in the mid-1970s. The Caravan had tabs; the Sahara did not.</p>
 
-    <p>Richard Polt's <em>The Classic Typewriter Page presents Writers and their Typewriters</em> web page lists the following writers as users of  Tippa models: Noel Coward (Gossen Pilot), Jack Higgins (Adler Tippa), Erich Kästner (Gossen Tippa), Stanley Kubrick (Adler Tippa S), and Joe Orton (Adler Tippa). Given the ultraportable size, the solid feel, and the quality of the Gossen models, it surprises me that only two Gossen typewriter-using writers are noted.</p>
+    <p>Richard Polt's web page, <em>The Classic Typewriter Page presents Writers and their Typewriters</em>, lists the following writers as users of Tippa models:</p>
+
+      <ul>
+        <li>Noel Coward (Gossen Pilot)</li>
+        <li>Jack Higgins (Adler Tippa)</li>
+        <li>Erich Kästner (Gossen Tippa)</li>
+        <li>Stanley Kubrick (Adler Tippa S)</li>
+        <li>Joe Orton (Adler Tippa)</li>
+      </ul>
+
+    <p>Given the ultraportable size, the solid feel, and the quality of the Gossen models, it surprises me that only two Gossen typewriter-using writers are noted.</p>
+    
 
     <h3>Meanwhile, more from Gossen...</h3>
 
-    <p>In the 1970s, I regularly used typewriters. We'd always had several in the house when I was growing up. Sadly, I have long forgotten what brand I used when writing papers for my university classes in the first half of that decade. (I do recall that it had a plastic shell and case.) However, I well-remember leasing an <em>IBM Selectric</em> in 1978 and 1979 to produce master copies of catalog pages of scarce books that I had scouted and was offering for sale. I also bought a new <b>Gossen Super Pilot</b> light meter to help me properly expose my film while photographing those books to ilustrate my catalogs. Word Processing on a PC soon followed, but I still have—and use—that Gossen light meter.</p>
+    <p>In the 1970s, I regularly used typewriters. We always had several in the house when I was growing up. Though I have forgotten what brand I used when writing papers for university in the 70s, I do recall that it was a portable with a plastic shell and case.) However, I well-remember leasing an <em>IBM Selectric</em> in 1978 and 1979 to produce master copies of catalog pages of scarce books that I had scouted and was offering for sale. I also bought a new <b>Gossen Super Pilot</b> light meter to help me properly expose my film while photographing those books to ilustrate my catalogs. Word Processing on a PC soon followed, but I still have—and use—that Gossen light meter.</p>
 
     <h4>Interesting Tippa Examples:</h4>
 
@@ -139,7 +155,7 @@ const GossenTypewritersPage = ({ data }) => {
 
     <p className="source"><b>Davis, Will</b>. <em><a href="http://www.willdavis.org/Reiseschreibmaschinen2.html" target="_blank" rel="noopener noreferrer">Reiseschreibmaschinen 2:  The Tippa</a>.</em> An undated web page about the Gossen, Adler, and Triumph Tippas. Retrieved 03 Apr 2023. [It appears this page was originally illustrated with 16 photos; sadly it now only has placeholders for them.]</p>
 
-    <p className="source"><b>GMC Istruments</b>. <em><a href="https://www.gmc-instruments.de/en/company/about-us/history/" target="_blank" rel="noopener noreferrer">History of the Gossen Company</a>.</em> Retrieved 05 Apr 2023. [Gossen Company history on the website of the company that now (April 2023) owns it. Scroll to bottom of the page and open the <em>History in Detail: 1906 - 1954</em> tab.]</p>
+    <p className="source"><b>GMC Istruments</b>. <em><a href="https://www.gmc-instruments.de/en/company/about-us/history/" target="_blank" rel="noopener noreferrer">History of the Gossen Company</a>.</em> Retrieved 05 Apr 2023. [Gossen Company history on the website of the company that owns it as of April 2023. Scroll to bottom of the page and open the <em>History in Detail: 1906 - 1954</em> tab.]</p>
 
     <p className="source"><b>Messenger, Robert</b>. <em><a href="https://oztypewriter.blogspot.com/2012/01/gossen-tippa-mighty-mouse-of.html" target="_blank" rel="noopener noreferrer">The Gossen Tippa: The Mighty Mouse of Typewriters</a>.</em> Blog post dated 07 Jan 2012. Retrieved 03 Apr 2023. [This is the best discussion and comparison  of the various Tippa models that we have seen.]</p>
 
